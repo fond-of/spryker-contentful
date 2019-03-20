e919772d957189720af34ffc7a7d743d2e5ad0e8
Pascal Fischer <pascal.fischer@fondof.de>
2019-03-20 15:23:35 +0100

    remove api import

--- src/FondOfSpryker/Zed/Contentful/Business/Storage/Field/DefaultFieldMapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FondOfSpryker\Zed\Contentful\Business\Storage\Field;
-
-use FondOfSpryker\Zed\Contentful\Business\Storage\Text\TextFieldMapper;
-
-class DefaultFieldMapper extends TextFieldMapper
-{
-}
